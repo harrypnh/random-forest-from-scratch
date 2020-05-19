147222044af52170937dbca3ac87ee7d625428c4
Harry Pham <huanpham@protonmail.com>
2020-05-19 19:01:57 +0700

    random forest algorithm

--- randomForest.py
+++ randomForest.py
@@ -16,14 +16,11 @@ def bootstrapSample(dataFrame, bootstrapSize):
     randomIndices = numpy.random.randint(low = 0, high = len(dataFrame), size = bootstrapSize)
     return dataFrame.iloc[randomIndices]
 
-def createRandomForest(dataFrame, bootstrapSize, randomAttributes,
-                       randomSplits, forestSize = 20, treeMaxDepth = 1000):
+def createRandomForest(dataFrame, bootstrapSize, randomAttributes, randomSplits, forestSize = 20, treeMaxDepth = 1000):
     forest = []
     for i in range(forestSize):
         bootstrappedDataFrame = bootstrapSample(dataFrame, bootstrapSize)
-        decisionTree = buildDecisionTree(bootstrappedDataFrame, maxDepth = treeMaxDepth,
-                                         randomAttributes = randomAttributes,
-                                         randomSplits = randomSplits)
+        decisionTree = buildDecisionTree(bootstrappedDataFrame, maxDepth = treeMaxDepth, randomAttributes = randomAttributes, randomSplits = randomSplits)
         forest.append(decisionTree)
     return forest
 
